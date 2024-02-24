1aa25f77e6f5ac4a5400ee5a355a67bb7ec66487
Kartik <kartik80484@gmail.com>
2024-02-24 21:47:54 +0530

    Changes till 24 feb evening

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return(
+        <div>
+            <div>Logo</div>
+            <div>
+            <Link href="/">HomePage</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
